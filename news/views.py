20026c2bd41af4955bad01154c227c243660ad42
Ulukbek2424 <jenishov.ulukbek24@gmail.com>
2020-10-26 16:50:33 +0600

    class CreateView

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import News, Category
 from .forms import NewsForm
@@ -19,6 +19,12 @@ class HomeNews(ListView):
         return News.objects.filter(is_published=True)
 
 
+#def get_category(request, category_id):
+    #news  = News.objects.filter(category_id=category_id)
+    #category = Category.objects.get(pk=category_id)
+    #return render(request, 'news/category.html', {'news': news, 'category': #category})
+
+
 class NewsByCategory(ListView):
     model = News
     template_name = 'news/home_news_list.html'
@@ -34,28 +40,28 @@ class NewsByCategory(ListView):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
 
+#def view_news(request, news_id):
+    #news_item = get_object_or_404(News, pk=news_id)
+    #return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+
 class ViewNews(DetailView):
     model = News
     context_object_name = 'news_item'
 
 
-#def get_category(request, category_id):
-    #news  = News.objects.filter(category_id=category_id)
-    #category = Category.objects.get(pk=category_id)
-    #return render(request, 'news/category.html', {'news': news, 'category': #category})
+#def add_news(request):
+#    if request.method == 'POST':
+#        form = NewsForm(request.POST)
+#        if form.is_valid():
+#            return redirect(news)
+#    else:
+#        form = NewsForm()
+#    return render(request, 'news/add_news.html', {'form': form})
 
 
-def view_news(request, news_id):
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
 
-
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
